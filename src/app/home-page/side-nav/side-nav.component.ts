0fd743fe5f4087dade0be52ed7945c5f073b4e0f
abc <you@example.com>
2024-09-07 20:12:18 +0300

    first commit

--- src/app/home-page/side-nav/side-nav.component.ts
+++ src/app/home-page/side-nav/side-nav.component.ts
@@ -1,18 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, model, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
   standalone: true,
-  imports: [MatIconModule, RouterLink],
+  imports: [MatIconModule, RouterLink, FormsModule],
   templateUrl: './side-nav.component.html',
   styleUrl: './side-nav.component.css',
 })
 export class SideNavComponent {
+  router = inject(Router);
+
   showVertical = signal(false);
+  searchFieldValue = model('');
 
   showVerticalList() {
     this.showVertical.set(!this.showVertical());
+  }
+
+  bind() {
+    this.router.navigate(['search/', this.searchFieldValue()]);
+    this.searchFieldValue.set('');
   }
 }
